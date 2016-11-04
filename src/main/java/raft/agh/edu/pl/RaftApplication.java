e3e0fd65192cd349e9397a28392b9893b34389ab
quirell <maxiol3@yahoo.com>
2016-11-04 09:50:59 +0100

    generated maven project

---
+++ src/main/java/raft/agh/edu/pl/RaftApplication.java
@@ -0,0 +1,12 @@
+package raft.agh.edu.pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaftApplication.class, args);
+	}
+}
